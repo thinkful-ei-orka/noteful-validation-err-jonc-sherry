6593a7951ef48ddbfb69647bb96cd247cf96ebdc
Sherry McCarty <sherrydmccarty@gmail.com>
2020-05-21 15:20:22 -0400

    added AddNote form HTML

---
+++ src/AddNote/AddNote.js
@@ -0,0 +1,8 @@
+import React from 'react';
+<fieldset>
+  <legend>Add New Note</legend>
+  <label htmlFor="note-name">Note Name</label>
+  <input type="text" name="note-name" id="note-name" placeholder="Note Name" value=""></input>
+  <label htmlFor="note-content">Note Content</label>
+  <input type="textarea" name="note-content" id="note-content" placeholder="Content" value=""></input>
+</fieldset>
